a6f70cc77cacaddaa095aa86b3e2d6d38c9c38c1
OneRealUni <43456208+OneRealUni@users.noreply.github.com>
2019-10-12 03:09:06 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+from scripts import tabledef
+from scripts import forms
+from scripts import helpers
+from flask import Flask, redirect, url_for, render_template, request, session
+import json
+import sys
+import os
+
+import stripe
+import pandas as pd
+from werkzeug.utils import secure_filename
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+
+os.environ['STRIPE_PUBLISHABLE_KEY'] = 'pk_test_fTyhAbPwg1hCNfYk6ssbRov800ayYIx6J7'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_z7o5gb3ZW2T0G2eOmjyBhdvT00KP6coj39'
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)  # Generic key for dev purposes only
+
+
+stripe_keys = {
+  'secret_key': os.environ['STRIPE_SECRET_KEY'],
+  'publishable_key': os.environ['STRIPE_PUBLISHABLE_KEY']
+}
+
+stripe.api_key = stripe_keys['secret_key']
+
+# ======== Routing =========================================================== #
+# -------- Login ------------------------------------------------------------- #
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = request.form['password']
+            if form.validate():
+                if helpers.credentials_valid(username, password):
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Login successful'})
+                return json.dumps({'status': 'Invalid user/pass'})
+            return json.dumps({'status': 'Both fields required'})
+        return render_template('login.html', form=form)
+    user = helpers.get_user()
+    return render_template('home.html', user=user)
+
+
+# -------- Signup ---------------------------------------------------------- #
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = helpers.hash_password(request.form['password'])
+            email = request.form['email']
+            if form.validate():
+                if not helpers.username_taken(username):
+                    helpers.add_user(username, password, email)
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Signup successful'})
+                return json.dumps({'status': 'Username taken'})
+            return json.dumps({'status': 'User/Pass required'})
+        return render_template('login.html', form=form)
+    return redirect(url_for('login'))
+
+
+# -------- Settings ---------------------------------------------------------- #
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if session.get('logged_in'):
+        if request.method == 'POST':
+            password = request.form['password']
+            if password != "":
+                password = helpers.hash_password(password)
+            email = request.form['email']
+            helpers.change_user(password=password, email=email)
+            return json.dumps({'status': 'Saved'})
+        user = helpers.get_user()
+        return render_template('settings.html', user=user)
+    return redirect(url_for('login'))
+
+# -------- Charge ---------------------------------------------------------- #
+@app.route('/charge', methods=['POST'])
+def charge():
+    if session.get('logged_in'):
+        user = helpers.get_user()
+        try:
+            amount = 1000   # amount in cents
+            customer = stripe.Customer.create(
+                email= user.email,
+                source=request.form['stripeToken']
+            )
+            stripe.Charge.create(
+                customer=customer.id,
+                amount=amount,
+                currency='usd',
+                description='Discount Optimizer Charge'
+            )
+            helpers.change_user(payment=helpers.payment_token())
+            user.active = True
+            return render_template('home.html', user=user)
+        except stripe.error.StripeError:
+            return render_template('error.html')
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+
+# ======== Main ============================================================== #
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
