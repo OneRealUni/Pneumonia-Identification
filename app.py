b947604d2a35a190a2d1874fe6f9d0149cb0d3e4
OneRealUni <43456208+OneRealUni@users.noreply.github.com>
2019-10-21 02:57:15 +0100

    Analyse monule
    Analyse

--- app.py
+++ app.py
@@ -110,6 +110,14 @@ def charge():
         except stripe.error.StripeError:
             return render_template('error.html')
 
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
